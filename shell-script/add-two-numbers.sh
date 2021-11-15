4feee86b2e33c7934c78adaafb5edc393be8b84c
nagendra464 <nagendra@gmail.com>
2021-11-15 11:16:15 +0530

    shell

--- shell-script/add-two-numbers.sh
+++ shell-script/add-two-numbers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 read -p "Enter the first number:" num1
-read -p " Enter the second number:" num2
+read -p "Enter the second number:" num2
 sum=$(($num1 + $num2))
 echo "Adding of two numbers is: $sum"
