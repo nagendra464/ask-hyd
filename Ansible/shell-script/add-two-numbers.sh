a17e0c22d947858da85fe0918d7cfa035c49306e
nagendra464 <nagendra.bheemuni@gmail.com>
2021-11-15 05:42:09 +0000

    shell script

--- Ansible/shell-script/add-two-numbers.sh
+++ Ansible/shell-script/add-two-numbers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 read -p "Enter the first number:" num1
 read -p " Enter the second number:" num2
-add=(($num1 + $num2))
-echo "Adding of two numbers is: $add
+sum=$(($num1 + $num2))
+echo "Adding of two numbers is: $sum"
